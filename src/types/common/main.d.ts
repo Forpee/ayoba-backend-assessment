deeeb6ae4b55a9d3553b16a31320dba67fe9a7f2
Forpee <khalilhassam3@gmail.com>
2022-10-25 14:04:18 +0200

    added types

---
+++ src/types/common/main.d.ts
@@ -0,0 +1,17 @@
+interface Track {
+    id: string;
+    name: string;
+    album: string;
+    artist: string;
+    duration: number;
+    artwork: string;
+    audio: string;
+}
+
+interface Playlist {
+    id: string;
+    name: string;
+    creator: string;
+    playTime: number;
+    trackList: Track[];
+}
